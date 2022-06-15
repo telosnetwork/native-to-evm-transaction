fba2d4ace35238d5803cd521eca4170cbc0e65c9
Thomas Cuvillier <titiomm@gmail.com>
2022-06-15 19:00:58 +0200

    Update serializeEVMTransaction.js

--- scripts/serializeEVMTransaction.js
+++ scripts/serializeEVMTransaction.js
@@ -3,51 +3,57 @@ import fetch from "node-fetch";
 import  Transaction from '@ethereumjs/tx'
 import  {BigNumber, ethers}  from  'ethers';
 
+// PARTIAL ABI W/ THE METHOD WE WANT TO CALL
 const contractAbi = [
     {
         "inputs": [
             {
-                "internalType": "uint256",
-                "name": "_lockDuration",
-                "type": "uint256"
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
             }
         ],
-        "name": "setLockDuration",
+        "name": "helloWorld",
         "outputs": [],
         "stateMutability": "nonpayable",
         "type": "function"
     }
 ]
 
-const provider = ethers.getDefaultProvider();
 const nativeAccount = "prods.evm";
 const contractAddress = "0x20027f1e6f597c9e2049ddd5ffb0040aa47f6135";
+
+const provider = ethers.getDefaultProvider();
 const contract = new ethers.Contract(contractAddress, contractAbi, provider);
-
 const evmApi = new TelosEvmApi({
-        endpoint: "https://testnet.telos.net",
-        chainId: '41',
-        ethPrivateKeys: [],
-        fetch: fetch,
-        telosContract: 'eosio.evm',
-        telosPrivateKeys: []
-    })
+    endpoint: "https://testnet.telos.net",
+    chainId: '41',
+    ethPrivateKeys: [],
+    fetch: fetch,
+    telosContract: 'eosio.evm',
+    telosPrivateKeys: []
+});
+(async () => {
 
-
-;(async () => {
     const evmAccount = await evmApi.telos.getEthAccountByTelosAccount(nativeAccount);
-let linkedAddress = evmAccount.address;
-const nonce = parseInt(await evmApi.telos.getNonce(linkedAddress), 16);
-const feeData = await provider.getFeeData()
-const gasPrice = BigNumber.from(`0x${await evmApi.telos.getGasPrice()}`)
-// POPULATE TRANSACTION FROM ARGS
-let unsignedTrx =  await contract.populateTransaction.setLockDuration("3600");
-unsignedTrx.nonce = nonce;
-unsignedTrx.gasLimit = BigNumber.from(`0xA0F4`);
-unsignedTrx.gasPrice = gasPrice;
-let raw = await ethers.utils.serializeTransaction(unsignedTrx);
-raw = raw.replace(/^0x/, '')
-console.log("Transaction: ", raw);
-console.log("Sender: ", linkedAddress.replace(/^0x/, ''));
-console.log("Cleos Command: ",  'cleos --url https://testnet.telos.caleos.io/ push action eosio.evm raw \'{"ram_payer": '+nativeAccount+', "tx": "'+ raw +'" , "estimate_gas": false, "sender": "'+ linkedAddress.replace(/^0x/, '') +'"}\' -p ' + nativeAccount);
+    var linkedAddress = evmAccount.address;
+    const nonce = parseInt(await evmApi.telos.getNonce(linkedAddress), 16);
+    const feeData = await provider.getFeeData()
+    const gasPrice = BigNumber.from(`0x${await evmApi.telos.getGasPrice()}`)
+
+    // POPULATE TRANSACTION
+    var unsignedTrx =  await contract.populateTransaction.helloWorld("MY GREAT NAME");
+    unsignedTrx.nonce = nonce;
+    unsignedTrx.gasLimit = BigNumber.from(`0xA0F4`);
+    unsignedTrx.gasPrice = gasPrice;
+
+    // SERIALIZE IT
+    var raw = await ethers.utils.serializeTransaction(unsignedTrx);
+    raw = raw.replace(/^0x/, '');
+
+    // PRINT IT OUT
+    console.log("Raw EVM Transaction: ", raw);
+    console.log("EVM Sender: ", linkedAddress.replace(/^0x/, ''));
+    console.log("Cleos Command: ",  'cleos --url https://testnet.telos.caleos.io/ push action eosio.evm raw \'{"ram_payer": '+nativeAccount+', "tx": "'+ raw +'" , "estimate_gas": false, "sender": "'+ linkedAddress.replace(/^0x/, '') +'"}\' -p ' + nativeAccount);
+
 })()
