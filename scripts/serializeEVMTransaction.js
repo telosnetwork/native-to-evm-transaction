f49dd5cb74c6c2e49d16461119fac19163c748cf
Thomas Cuvillier <titiomm@gmail.com>
2022-06-15 18:00:00 +0200

    Update serializeEVMTransaction.js

--- scripts/serializeEVMTransaction.js
+++ scripts/serializeEVMTransaction.js
@@ -3,9 +3,6 @@ import fetch from "node-fetch";
 import  Transaction from '@ethereumjs/tx'
 import  {BigNumber, ethers}  from  'ethers';
 
-const nativeAccount = "prods.evm";
-const EVMContractAddress = "0x20027f1e6f597c9e2049ddd5ffb0040aa47f6135";
-
 const contractAbi = [
     {
         "inputs": [
@@ -36,6 +33,8 @@ const contractAbi = [
 ]
 
 const provider = ethers.getDefaultProvider();
+const account = "prods.evm";
+const contractAddress = "0x20027f1e6f597c9e2049ddd5ffb0040aa47f6135";
 const contract = new ethers.Contract(contractAddress, contractAbi, provider);
 
 const evmApi = new TelosEvmApi({
@@ -50,15 +49,17 @@ const evmApi = new TelosEvmApi({
 const myArgs = process.argv.slice(2);
 
 ;(async () => {
-    const evmAccount = await evmApi.telos.getEthAccountByTelosAccount(nativeAccount);
+    const evmAccount = await evmApi.telos.getEthAccountByTelosAccount(account);
     let linkedAddress = evmAccount.address;
     const nonce = parseInt(await evmApi.telos.getNonce(linkedAddress), 16);
     const feeData = await provider.getFeeData()
     const gasPrice = BigNumber.from(`0x${await evmApi.telos.getGasPrice()}`)
-    let unsignedTrx = null;
     if(myArgs.length < 2){
+        console.log("This script requires 2 arguments: node serializeEVMTransaction.js METHOD PARAMS")
         return;
     }
+    // POPULATE TRANSACTION FROM ARGS
+    let unsignedTrx = null;
     switch(myArgs[0].toString()){
         case "setLockDuration":
             unsignedTrx = await contract.populateTransaction.setLockDuration(myArgs[1]);
@@ -67,15 +68,17 @@ const myArgs = process.argv.slice(2);
             unsignedTrx = await contract.populateTransaction.setMaxDeposits(myArgs[1]);
             break;
         case "transferOwnership":
-            unsignedTrx = await contract.populateTransaction.transferOwnership(myArgs[1]);
+            unsignedTrx = await contract.populateTransaction.transferOwnership(myArgs[1].replace(/^0x/, ''));
     }
     if(unsignedTrx == null){
-        return ;
+        console.log("Method not found");
+        return;
     }
     unsignedTrx.nonce = nonce;
     unsignedTrx.gasLimit = BigNumber.from(`0xA0F4`);
     unsignedTrx.gasPrice = gasPrice;
     let raw = await ethers.utils.serializeTransaction(unsignedTrx);
     raw = raw.replace(/^0x/, '')
-    console.log(raw);
+    console.log("Transaction: ", raw);
+    console.log("Cleos Command: ",  'cleos --url https://testnet.telos.caleos.io/ push action eosio.evm raw \'{"ram_payer": prods.evm, "tx": '+ raw +' , "estimate_gas": false, "sender": "7c56101c01eaaece3d1bb330910c8e9183b39dbd"}\' -p prods.evm');
 })()
