6787a26c11ee8abfe02d5be08c0da36e5e5c1107
tomtomcrypto <titiomm@gmail.com>
2022-06-16 02:07:35 +0000

    refactor

--- serializeEVMTransaction.js
+++ serializeEVMTransaction.js
@@ -35,6 +35,7 @@ const contractAbi = [
 
 const nativeAccount = process.env.NATIVE_ACCOUNT_ADDRESS;
 const contractAddress = process.env.EVM_CONTRACT_ADDRESS;
+const amount = (100 * (10 ** 18)).toString();
 
 const provider = ethers.getDefaultProvider();
 const contract = new ethers.Contract(contractAddress, contractAbi, provider);
@@ -47,32 +48,29 @@ const evmApi = new TelosEvmApi({
     telosPrivateKeys: []
 });
 (async () => {
+    var evmAccount, linkedAddress, nonce;
+    try {
+        evmAccount = await evmApi.telos.getEthAccountByTelosAccount(nativeAccount);
+        linkedAddress = evmAccount.address;
+        nonce = parseInt(await evmApi.telos.getNonce(linkedAddress), 16);
+    } catch(e) {
+        console.log(e.message);
+        return;
+    }
 
-    try {
-        const evmAccount = await evmApi.telos.getEthAccountByTelosAccount(nativeAccount);
-    } catch(e) {
-        console.log(e.message);
-        return;
-    }
-    var linkedAddress = evmAccount.address;
-    try {
-        const nonce = parseInt(await evmApi.telos.getNonce(linkedAddress), 16);
-    } catch(e) {
-        console.log(e.message);
-        return;
-    }
     const feeData = await provider.getFeeData()
     const gasPrice = BigNumber.from(`0x${await evmApi.telos.getGasPrice()}`)
+    const gasLimit = (await contract.estimateGas.mint(process.env.EVM_MINT_TO_ADDRESS, amount)) * 5;
 
-    // POPULATE TRANSACTION
+// POPULATE TRANSACTION
     try {
-        var unsignedTrx =  await contract.populateTransaction.mint(process.env.EVM_MINT_TO_ADDRESS, 100);
+        var unsignedTrx =  await contract.populateTransaction.mint(process.env.EVM_MINT_TO_ADDRESS, amount);
     } catch(e) {
         console.log(e.message);
         return;
     }
     unsignedTrx.nonce = nonce;
-    unsignedTrx.gasLimit = BigNumber.from(`0xA0F4`);
+    unsignedTrx.gasLimit = gasLimit;
     unsignedTrx.gasPrice = gasPrice;
 
     // SERIALIZE IT
@@ -85,8 +83,8 @@ const evmApi = new TelosEvmApi({
     raw = raw.replace(/^0x/, '');
 
     // PRINT IT OUT
-    console.log("Raw EVM Transaction: ", raw);
-    console.log("EVM Sender: ", linkedAddress.replace(/^0x/, ''));
-    console.log("Cleos Command: ",  'cleos --url https://testnet.telos.caleos.io/ push action eosio.evm raw \'{"ram_payer": '+nativeAccount+', "tx": "'+ raw +'" , "estimate_gas": false, "sender": "'+ linkedAddress.replace(/^0x/, '') +'"}\' -p ' + nativeAccount);
+    console.log("SERIALIZED_TX: ", raw);
+    console.log("LINKED_ADDRESS: ", linkedAddress.replace(/^0x/, ''));
+    console.log("CLEOS COMMAND: ",  'cleos --url https://testnet.telos.caleos.io/ push action eosio.evm raw \'{"ram_payer": '+nativeAccount+', "tx": "'+ raw +'" , "estimate_gas": false, "sender": "'+ linkedAddress.replace(/^0x/, '') +'"}\' -p ' + nativeAccount);
 
 })()
